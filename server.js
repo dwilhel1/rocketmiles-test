2eecbed5b4bf44bb9784f09e034e69c3325eb21b
Douglas Wilhelm <dwilhelm@vacayhomeconnect.com>
2019-07-03 01:03:24 -0500

    Correct Express path configs

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(`${__dirname}'/dist/rocketmiles-test`));
+app.use(express.static('./dist/rocketmiles-test'));
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(`${__dirname}/dist/rocketmiles-test/index.html`));
+  res.sendFile(path.join('./dist/rocketmiles-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
