19ba79307e612ecb273c793c8f95abcfe805cc6a
Douglas Wilhelm <dwilhelm@vacayhomeconnect.com>
2019-07-05 17:06:34 -0500

    Refactor app component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Subject } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
+import { Subject} from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { HotelService } from './services/hotel/hotel.service';
 
-import { HotelResponse } from './models/hotel/response/hotel.model';
+import { Hotel, HotelResponse } from './models/hotel/response/hotel.model';
+import { CurrencyEnum } from './enum/currency.enum';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ import { HotelResponse } from './models/hotel/response/hotel.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  public currencyEnum = CurrencyEnum;
   public hotelResponse: HotelResponse;
+  public tableData: MatTableDataSource<Hotel>;
   public hotelSearchFormGroup: FormGroup;
   private unsubscribe: Subject<void> = new Subject();
 
@@ -30,8 +34,13 @@ export class AppComponent implements OnInit, OnDestroy {
     this.hotelService.getHotels().pipe(
       takeUntil(this.unsubscribe),
     ).subscribe((response: HotelResponse) => {
-      this.hotelResponse = response;
-      this.onChanges();
+      if (response) {
+        this.hotelResponse = response;
+        this.configureTableData();
+        this.onChanges();
+      } else {
+        this.hotelSearchFormGroup.disable();
+      }
     });
   }
 
@@ -42,13 +51,75 @@ export class AppComponent implements OnInit, OnDestroy {
 
   public resetSearch(): void {
     this.hotelSearchFormGroup.reset();
+    this.configureTableData();
   }
 
   public onChanges(): void {
-    this.hotelSearchFormGroup.valueChanges.pipe(
+    this.hotelSearchFormGroup.controls.currency.valueChanges.pipe(
       takeUntil(this.unsubscribe),
-    ).subscribe(result => {
-      console.log(result);
+    ).subscribe(currencyChanges => {
+      if (currencyChanges) {
+        this.sortData(currencyChanges);
+      }
     });
+
+    this.hotelSearchFormGroup.controls.hotelSearch.valueChanges.pipe(
+      takeUntil(this.unsubscribe),
+    ).subscribe(searchChanges => {
+      this.filterData(searchChanges);
+    });
+  }
+
+  private configureTableData(): void {
+    this.tableData = new MatTableDataSource(Array.from(this.hotelResponse.results.hotels));
+  }
+
+  private sortData(key: CurrencyEnum): void {
+    switch (key) {
+      case CurrencyEnum.DESC:
+      case CurrencyEnum.ASC:
+        // Sort according to lowestAveragePrice.amount
+        this.tableData.data = this.tableData.data.sort((a, b) => {
+          const nameA = a.lowestAveragePrice.amount;
+          const nameB = b.lowestAveragePrice.amount;
+
+          if (nameA < nameB) {
+            if (key === CurrencyEnum.ASC) {
+              return 1
+            } else {
+              return -1;
+            }
+          }
+          if (nameA > nameB) {
+            if (key === CurrencyEnum.ASC) {
+              return -1
+            } else {
+              return 1;
+            }
+          }
+        });
+        break;
+      case CurrencyEnum.RECOMMENDED:
+        // TODO: Determine how recommended list is to be structured; control is disabled for now
+        break;
+      default:
+        break;
+    }
+  }
+
+  private filterData(search: string): void {
+    if (search) {
+      this.tableData.data = this.hotelResponse.results.hotels.filter(hotel => {
+        const hotelName = hotel.hotelStaticContent.name.trim().toLowerCase();
+        const hotelNameSearch = search.trim().toLowerCase();
+
+        return ~hotelName.indexOf(hotelNameSearch)
+      });
+      if (this.hotelSearchFormGroup.controls.currency.value) {
+        this.sortData(this.hotelSearchFormGroup.controls.currency.value);
+      }
+    } else {
+      this.configureTableData();
+    }
   }
 }
---
+++ src/app/enum/currency.enum.ts
@@ -0,0 +1,5 @@
+export enum CurrencyEnum {
+    ASC = 'ASC',
+    DESC = 'DESC',
+    RECOMMENDED = 'RECOMMENDED',
+}
